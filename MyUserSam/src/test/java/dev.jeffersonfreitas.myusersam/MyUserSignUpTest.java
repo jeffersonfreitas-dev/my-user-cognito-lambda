29d9821b2e1f4044f141d8f98ae10bc450b971c1
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-04-17 16:45:21 -0300

    Create a simple project

---
+++ MyUserSam/src/test/java/dev.jeffersonfreitas.myusersam/MyUserSignUpTest.java
@@ -0,0 +1,22 @@
+package dev.jeffersonfreitas.myusersam;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MyUserSignUpTest {
+//  @Test
+  public void successfulResponse() {
+    MyUserSignUp app = new MyUserSignUp();
+    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
